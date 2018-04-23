bfabb0e7f1e0815acf2f0550f1469623dd824349
Satbirajah <38457013+Satbirajah@users.noreply.github.com>
2018-04-23 14:02:21 +0200

    ich pushe lg sapi

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,67 +1,93 @@
 //Variablem deklaration; 
-var bmiWert;
-var kaloren;
+var bmiWert = 0;
+var rmr = 0;
+var kalorien = 0;
 
-var geschlecht;
 var istMann = false;
 var istFrau = false;
 
-var alter; 
-
-var bewegung;
-var bewegtWenig = false;
-var bewegtMittel = false;
-var bewgegtViel = false;
-
+var bewegtWenig = 1.2;
+var bewegtMittel = 1.55;
+var bewgegtViel = 1.9;
 var gewicht;
 
 var groesse;
 
-var hatFehler= false;
-
+var hatFehler = false;
+//Männer: RMR = (10 × Gewicht in kg) + (6,25 × Größe in cm) – (5 × Alter) + 5
+//Frauen: RMR = (10 × Gewicht in kg) + (6,25 × Größe in cm) – (5 × Alter) – 161
 //Funktionen 
 //BMI Rechner 
-function bmiRechner(){
-	gewicht();
-	groesse(); 
-	if(!hatFehler){
-		bmiWert= gewicht/(groesse*groesse);
+function bmiRechner() {
+	gewichtBenutzer();
+	groesseBenutzer();
+	if (!hatFehler) {
+		bmiWert = gewicht / (groesse * groesse);
 		document.getElementById("bmiWert").innerHTML = bmiWert;
 	}
-	
-
 }
 //KalorienBedarfRechner
-function kalorienBedarfRechner(){
+function kalorienBedarfRechner() {
+    geschlechtBenutzer();
+    var gewicht = gewichtBenutzer();
+    var groesse = groesseBenutzer();
+    var alter = alterBenutzer();
+    var bewegung = bewegungBenutzer();
+    
+	groesse = groesse * 100;
+	if (istMann) {
+		rmr = (10 * gewicht) + (6.25 * groesse) - (5 * alter) + 5;
+	}
+	if (istFrau) {
+		rmr = (10 * gewicht) + (6.25 * groesse) - (5 * alter) - 161;
+
+	}
+	kalorien = rmr * bewegung;
+    document.getElementById("kalorien").innerHTML= kalorien;
 
 }
 //Hier Beginnt die Auswertung der Benutzereingaben
 //Bestimmung welches Geschlecht der Benutzer hat
-function geschlecht(){
-	geschlecht = document.querySelector('input[name="geschlecht"]:checked').value;
+function geschlechtBenutzer() {
+	var geschlecht = document.querySelector('input[name="geschlecht"]:checked').value;
+	if ( geschlecht == "mann"){
+		istMann = true;
+	}
+	if( geschlecht == "frau"){
+		istFrau = true;
+	}
 	
 }
 //Auswertung des Alters 
-function alter(){
-	alter = document.querySelector("#alter").value;
+function alterBenutzer() {
+	var alter = document.querySelector("#alter").value;
 	if(istEingabeZahl(alter)){
-		hatFehler=false;
+		hatFehler = false;
 	}
 	else{
 
-		hatFehler=true;
+		hatFehler = true;
 		var fehler ="Geben Sie bitte eine Zahl für ihr Alter an."
 		fehler(fehler);
 	}
 
 }
 //Auswertung wie viel sich der Benutzer Bewegt
-function bewegung(){
-	bewegung = document.querySelector('input[name="bewegung"]:checked').value;
+function bewegungBenutzer() {
+	var bewegung = document.querySelector('input[name="bewegung"]:checked').value;
+	if(bewegung=="wenig"){
+        return bewegtWenig;
+	}
+    if( bewegung === "mittel"){
+        return bewegtMittel;
+    }
+    if( bewegung === "viel"){
+        return bewgegtViel;
+    }
 }
 //Auswerten wie viel der Benutzer wiegt
-function gewicht(){
-	gewicht = document.querySelector("#gewicht_BMI").value;
+function gewichtBenutzer() {
+	gewicht = document.querySelector("#gewicht").value;
 	if(istEingabeZahl(gewicht)){
 		hatFehler=false;
 	}
@@ -74,8 +100,8 @@ function gewicht(){
 
 }
 //Auswerten wie gross der Benutzer 
-function groesse(){
-	groesse = document.querySelector("#groesse_BMI").value;
+function groesseBenutzer() {
+	groesse = document.querySelector("#groesse").value;
 	if(istEingabeZahl(groesse)){
 		hatFehler=false;
 	}
@@ -83,21 +109,21 @@ function groesse(){
 
 		hatFehler=true;
 		var fehler ="Geben Sie bitte eine Zahl für ihre Grösse an."
-		fehler(fehler);
+		fehlerMeldung(fehler);
 	}
 }
 //Validation von den Benutzer eingaben
 //Überprüfung ob der Benutzer eine Zahl eingegeben hat
-function istEingabeZahl(eingabe){
-	if(isInt(eingabe)){
+function istEingabeZahl(eingabe) {
+	if(Number.isInteger(eingabe)){
 		return true;
 	}
-	else{ss
+	else{
 		return false;
 	}
 }
 //Fehlermeldungen 
-function fehler(fehlermeldung){
+function fehlerMeldung(fehlermeldung) {
 	document.getElementById("fehler").innerHTML= fehlermeldung;
 }
 
